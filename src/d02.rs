d2d50b60ab28400029d086adfbf0c446558bd89b
Lang Martin <lang.martin@gmail.com>
2020-12-02 13:04:43 -0500

    day2, cleanup

--- src/d02.rs
+++ src/d02.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn valid(line:&str) -> bool {
+fn valid_part1(line:&str) -> bool {
     let policy_passwd:Vec<&str> = line.split(":").take(2).collect();
     let policy = policy_passwd[0];
     let passwd = policy_passwd[1];
@@ -18,11 +18,39 @@ fn valid(line:&str) -> bool {
     return count >= start && count <= end;
 }
 
-pub fn part1() -> usize {
+fn valid_part2(line:&str) -> bool {
+    let policy_passwd:Vec<&str> = line.split(":").take(2).collect();
+    let policy = policy_passwd[0];
+    let passwd = policy_passwd[1].trim();
+
+    let policy_fs:Vec<&str> = policy.split(" ").take(2).collect();
+    let range:Vec<&str> = policy_fs[0].split("-").collect();
+
+    let check = policy_fs[1].chars().next().unwrap();
+    let start = range[0].parse::<usize>().unwrap() - 1;
+    let end = range[1].parse::<usize>().unwrap() - 1;
+
+    let pwd:Vec<char> = passwd.chars().collect();
+
+    // dbg!(passwd, start, end, pwd[start], pwd[end]);
+
+    return (pwd[start] == check || pwd[end] == check) &&
+	!(pwd[start] == check && pwd[end] == check);
+}
+
+fn part_n(f: &dyn Fn(&str) -> bool) -> usize {
     let file = File::open("data/d02.txt").unwrap();
     return io::BufReader::new(file)
 	.lines()
 	.map(|l| l.unwrap())
-	.filter(|l| valid(l))
+	.filter(|l| f(l))
 	.count();
+}
+
+pub fn part1() -> usize {
+    return part_n(&valid_part1);
+}
+
+pub fn part2() -> usize {
+    return part_n(&valid_part2);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ mod d02;
 
 fn main() {
     dbg!(d02::part1());
-    // dbg!(d01::part2());
+    dbg!(d02::part2());
 }
